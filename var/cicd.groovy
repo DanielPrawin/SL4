b485210048dba4b90d0c0d69794d58a89ac56f5d
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-08-08 16:02:12 +0530

    version2

--- var/cicd.groovy
+++ var/cicd.groovy
@@ -1,17 +1,17 @@
 def gitRepo(repo)
 {
-  git 'https://github.com/DanielPrawin/${repo}.git'
+  git "https://github.com/DanielPrawin/${repo}.git"
 }
 def buildCreation()
 {
-  sh 'mvn package'
+  sh "mvn package"
 }
 def deploy(jobName,ip,context)
 {
-   sh 'scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war'
+   sh "scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
 def testing(jobName)
 {
- sh 'java -jar /var/lib/jenkins/workspace/${jobName}/testing.jar'
+ sh "java -jar /var/lib/jenkins/workspace/${jobName}/testing.jar"
 }
